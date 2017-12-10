6318621b117ea2a12d46aa967af0e6f040620062
German Mesky <drgmes@gmail.com>
2017-12-10 06:39:41 +0200

    Changed unlock-popup styles

--- app/components/Account/AccountSelector.jsx
+++ app/components/Account/AccountSelector.jsx
@@ -119,23 +119,24 @@ class AccountSelector extends React.Component {
         return (
             <div className="account-selector" style={this.props.style}>
                 <div className="content-area">
-                    {this.props.label ? (
-                    <div className="header-area">
-                        {error ? null : <label className="right-label"><span>{member_status}</span> &nbsp; <span>{lookup_display}</span></label>}
-                        <Translate className="left-label" component="label" content={this.props.label}/>
-                    </div>) : null}
+                    {/*{this.props.label ? (*/}
+                    {/*<div className="header-area">*/}
+                        {/*{error ? null : <label className="right-label"><span>{member_status}</span> &nbsp; <span>{lookup_display}</span></label>}*/}
+                        {/*<Translate className="left-label" component="label" content={this.props.label}/>*/}
+                    {/*</div>) : null}*/}
                     <div className="input-area">
-                        <div className="inline-label input-wrapper">
-                            {type === "pubkey" ? <div className="account-image"><Icon name="key" size="4x"/></div> :
-                            this.props.hideImage ? null : <AccountImage size={{height: this.props.size || 80, width: this.props.size || 80}}
-                                account={this.props.account ? this.props.account.get("name") : null} custom_image={null}/>}
+                        <div className="inline-label input-wrapper login">
+                            {/*{type === "pubkey" ? <div className="account-image"><Icon name="key" size="4x"/></div> :*/}
+                            {/*this.props.hideImage ? null : <AccountImage size={{height: this.props.size || 80, width: this.props.size || 80}}*/}
+                                {/*account={this.props.account ? this.props.account.get("name") : null} custom_image={null}/>}*/}
+                                <Icon name="user" className="icon-14px" />
                                 <input
-                                    style={{textTransform: "lowercase", fontVariant: "initial"}}
+                                    style={{fontVariant: "initial"}}
                                     type="text"
                                     name="username"
                                     id="username"
                                     value={this.props.accountName || ""}
-                                    placeholder={this.props.placeholder || counterpart.translate("account.name")}
+                                    placeholder={this.props.placeholder || counterpart.translate("header.unlock_short")}
                                     ref="user_input"
                                     onChange={this.onInputChanged.bind(this)}
                                     onKeyDown={this.onKeyDown.bind(this)}
