fa70d4c1f2048ce93c9db8573bcbb1625cf20b94
Zurab Shivarbidze <zybpwnz@gmail.com>
2017-12-14 17:47:09 +0200

    markup top-apps

--- app/components/TopApps/TopApps.jsx
+++ app/components/TopApps/TopApps.jsx
@@ -9,7 +9,7 @@ class TopApps extends React.Component {
     render() {
 
         return (
-            <div className="grid-block page-layout flex-start">
+            <div className="grid-block flex-start">
                 <div className="grid-block shrink vertical medium-horizontal large-10 large-offset-1 top-apps-list">
                     <div className="grid-block shrink vertical medium-horizontal">
                         <div className="generic-bordered-box">
@@ -21,7 +21,7 @@ class TopApps extends React.Component {
 
                         <div className="item">
                             <div className="img__block">
-                                <img width={170} height={170} src={`${__BASE_URL__}mapala.png`}/>
+                                <img src="http://via.placeholder.com/170x170" alt="Mapala"/>
                             </div>
 
                             <div className="info__block">
@@ -49,25 +49,20 @@ class TopApps extends React.Component {
 
                                 <a href="https://mapala.net" className="item__link">https://mapala.net</a>
                             </div>
-                            <div className="social__block">
-                                <Icon name="blockchain_grdient" size="5x" fillClass="fill-black"/>
-                                <Icon name="blockchain_grdient" size="5x" fillClass="fill-black"/>
-                                <Icon name="blockchain_grdient" size="5x" fillClass="fill-black"/>
-
-
-                                "github-sign",
-                                "telegram",
-                                "vkontakte-logo"]
-                            </div>
+                            <ul className="social__block">
+                                <li><Icon name="vkontakte-logo" size="2x" fillClass="fill-black"/></li>
+                                <li><Icon name="github-sign" size="2x" fillClass="fill-black"/></li>
+                                <li><Icon name="telegram" size="2x" fillClass="fill-black"/></li>
+                            </ul>
                         </div>
 
 
                         <div className="item">
                             <div className="img__block">
-                                <img src={`${__BASE_URL__}aigents.png`} alt=""/>
+                                <img src="http://via.placeholder.com/170x170" alt="Aigents"/>
                             </div>
                             <div className="info__block">
-                                <h3 className="item__title">Mapala</h3>
+                                <h3 className="item__title">Aigents</h3>
                                 <p className="item__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
                                     do
                                     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -91,9 +86,11 @@ class TopApps extends React.Component {
 
                                 <a href="https://mapala.net" className="item__link">https://mapala.net</a>
                             </div>
-                            <div className="social__block">
-
-                            </div>
+                            <ul className="social__block">
+                                <li><Icon name="vkontakte-logo" size="2x" fillClass="fill-black"/></li>
+                                <li><Icon name="github-sign" size="2x" fillClass="fill-black"/></li>
+                                <li><Icon name="telegram" size="2x" fillClass="fill-black"/></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
